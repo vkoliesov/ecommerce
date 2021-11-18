64d8ed46d50614f25ee99b1002f35abc7478bf01
vk <vk@ucode.world>
2021-11-18 19:00:12 +0200

    make detail product, add cart template

--- shop/views.py
+++ shop/views.py
@@ -13,5 +13,14 @@ def home(request, category_slug=None):
     return render(request, 'home.html', {'category': category_page, 'products': products})
 
 
-def product(request):
-    return render(request, 'product.html')
+def product(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug,
+        slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
+
+
+def cart(request):
+    return render(request, 'cart.html')
